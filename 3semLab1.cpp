fb9e3f5a21e66ed1159c2137beb2b83647303375
user <user@WIN-KLKMDAT4B5D>
2025-09-07 19:52:38 +0300

    testing

--- 3semLab1.cpp
+++ 3semLab1.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+class Object
+{
+	int serialNumber;
+	int quantity;
+
+	Object()
+	{
+		serialNumber = quantity;
+	}
+
+public:
+	void printSerialNumber();
+};
+
 int main()
 {
-
+	Object* obj;
 
 	return 0;
+}
+
+void Object::printSerialNumber()
+{
+	cout << serialNumber << endl;
 }
