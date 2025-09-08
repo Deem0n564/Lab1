e97d7b57e89fd1f994d6396b19c87807900e1f47
user <user@WIN-KLKMDAT4B5D>
2025-09-08 19:43:19 +0300

    .

--- 3semLab1.cpp
+++ 3semLab1.cpp
@@ -18,7 +18,22 @@ public:
 
 int main()
 {
+	int choise;
 	Object* obj;
+
+	do
+	{
+		cout << "\tMENU\n 1. Add object.\n 2. Delete object.\n 3. Exit\nYour choise is: ";
+			choise = cin.get();
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(100, '\n');
+
+				cout << " Incorrect! Please, try again\n";
+			}
+		
+	} while (choise != 3);
 
 	return 0;
 }
