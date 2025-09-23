f6bcc3e09190eac85c6e75bcd63449198eee8c48
user <user@WIN-KLKMDAT4B5D>
2025-09-23 12:35:39 +0300

    esche raz

--- functions.cpp
+++ functions.cpp
@@ -49,7 +49,7 @@ void addObject(Object*& obj, int& size)
 
 }
 
-void printObjects(const Object *obj, int* size)
+void printObjects(const Object *obj, const int* size)
 {
 	for (int i = 0; i < *size; i++)
 	{
